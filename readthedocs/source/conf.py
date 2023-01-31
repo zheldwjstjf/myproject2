ab87dcfb5cf8762397128dbededaab214d57267b
root <zheldwjstjf@gmail.com>
2023-01-31 14:45:39 +0000

    fix config : extensions

--- readthedocs/source/conf.py
+++ readthedocs/source/conf.py
@@ -18,7 +18,7 @@ extensions = ['autoapi.extension']
 
 # Document Python Code
 autoapi_type = 'python'
-autoapi_dirs = ['/home/docs/checkouts/readthedocs.org/user_builds/jacks-test-doc/envs/latest/lib/python3.7/site-packages/autoapi/extension.py']
+autoapi_dirs = ['/home/docs/checkouts/readthedocs.org/user_builds/jacks-test-doc/envs/latest/lib/python3.7/site-packages/autoapi/', 'extension']
 
 templates_path = ['_templates']
 exclude_patterns = []
